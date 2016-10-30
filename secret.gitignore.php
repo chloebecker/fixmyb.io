5b4e744d322323de0830fcddb1b7fed5498dc3ab
Chloe Becker <chloe.a.becker@gmail.com>
2016-10-29 23:33:50 -0500

    removed unecessary docs

--- secret.gitignore.php
+++
@@ -1,4 +0,0 @@
-'oauth_access_token' => "792463225463853061-P5ZDTgnzgkroQtYu3QkPR5wn9GSmfLA",
-    'oauth_access_token_secret' => "HQVGmAORW0RTZFDdg0CSWuRShQ7THCJnsAh0mPnOKNR6O",
-    'consumer_key' => "o7QBNAyTr2odzOdGSIrBENTEY",
-    'consumer_secret' => "uqafomtqM8XZTxt8gC7Sr7lmZfJIHjqnTUvJp4yVv74f7PwfQw"
