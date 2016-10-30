a50012c372247000549faf7f3ddea60f2131b0d8
LiamKhuen <liamkhuen@gmail.com>
2016-10-30 09:13:08 -0500

    Converted to PHP
    Converted dashboard to php.

---
+++ liamUIWork/userDashboard.php
@@ -0,0 +1,160 @@
+<?php
+
+print "<!DOCTYPE html>\n".
+ "<html lang=\"en\">\n".
+ "\n".
+ "<head>\n".
+ "\n".
+ "<title> User Dashboard </title>\n".
+ "<meta charset=\"utf-8\">\n".
+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n".
+ "\n".
+ "<<<<<<< HEAD\n".
+ "<!--Import jQuery before materialize.js-->\n".
+ "<script type=\"text/javascript\" src=\"https://code.jquery.com/jquery-2.1.1.min.js\"></script>\n".
+ "<!-- Compiled and minified CSS -->\n".
+ "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css\">\n".
+ "<!-- Compiled and minified JavaScript -->\n".
+ "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js\"></script>\n".
+ "<!--Import materialize.css-->\n".
+ "<link type=\"text/css\" rel=\"stylesheet\" href=\"css/materialize.min.css\"  media=\"screen,projection\"/>\n".
+ "<!--Let browser know website is optimized for mobile-->\n".
+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n".
+ "<!-- Google Web Fonts -->\n".
+ "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">\n".
+ "<!--Import Google Icon Font-->\n".
+ "<link href=\"http://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\n".
+ "\n".
+ "<!-- My stylesheet(s) -->\n".
+ "\n".
+ "<link rel=\"stylesheet\" href=\"./userDashboard_2.css\">\n".
+ "\n".
+ "<!-- Materialize Helper File -->\n".
+ "\n".
+ "<script src=\"./materializeHelper.js\"></script>\n".
+ "=======\n".
+ "<!-- Bootstrap -->\n".
+ "\n".
+ "<link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">\n".
+ "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>\n".
+ "<script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\"></script>\n".
+ "\n".
+ "<!-- My stylesheet(s) -->\n".
+ "\n".
+ "<link rel=\"stylesheet\" href=\"./userDashboard.css\">\n".
+ ">>>>>>> master\n".
+ "\n".
+ "</head>\n".
+ "\n".
+ "\n".
+ "<body>\n".
+ "\n".
+ "<<<<<<< HEAD\n".
+ "<nav>\n".
+ "<div class=\"nav-wrapper\">\n".
+ "  <a href=\"#!\" class=\"brand-logo center\"><i class=\"material-icons\">content_paste</i>Fix My Bio</a>\n".
+ "  <ul class=\"right hide-on-med-and-down\">\n".
+ "<!-- Write search function? -->\n".
+ "    <li><a href=\"sass.html\"><i class=\"material-icons\">search</i></a></li>\n".
+ "\n".
+ "    <li><a href=\"#\"><i class=\"material-icons\">dashboard</i></a></li>\n".
+ "    <li><a href=\"./topProf\"><i class=\"material-icons\">whatshot</i></a></li>\n".
+ "    <li><a href=\"./profile\"><i class=\"material-icons\">face</i></a></li>\n".
+ "    <li><a href=\"./message\"><i class=\"material-icons\">chat</i></a></li>\n".
+ "\n".
+ "    <!-- Create notification popup -->\n".
+ "\n".
+ "    <!-- Modal Trigger -->\n".
+ "    <!-- Dropdown Trigger -->\n".
+ "<a class='dropdown-button btn' data-beloworigin=\"true\" href='#' data-activates='notificationDD'>Notifications</a>\n".
+ "\n".
+ "<!-- Dropdown Structure -->\n".
+ "<ul id='notificationDD' class='dropdown-content'>\n".
+ "  <li><a href=\"#!\">You got mail!</a></li>\n".
+ "  <li><a href=\"#!\">You were reviewed.</a></li>\n".
+ "  <li><a href=\"#!\">You were reviewed.</a></li>\n".
+ "</ul>\n".
+ "\n".
+ "  </ul>\n".
+ "</div>\n".
+ "\n".
+ "</nav>\n".
+ "\n".
+ "      <div class=\"row\">\n".
+ "        <div class=\"col s2 m1\">\n".
+ "          <img class=\"materialboxed\" width=\"75\" src=\"./baseIcon.png\">\n".
+ "        </div>\n".
+ "        <div class=\"col s10 m5\">\n".
+ "          <div class=\"card blue-grey darken-1\">\n".
+ "            <div class=\"card-content white-text\">\n".
+ "              <span class=\"card-title\">Card Title</span>\n".
+ "              <p>I am a very simple card. I am good at containing small bits of information.\n".
+ "              I am convenient because I require little markup to use effectively.</p>\n".
+ "            </div>\n".
+ "            <div class=\"card-action\">\n".
+ "              <a href=\"#\">This is a link</a>\n".
+ "              <a href=\"#\">This is a link</a>\n".
+ "            </div>\n".
+ "          </div>\n".
+ "        </div>\n".
+ "      </div>\n".
+ "=======\n".
+ "  <div class=\"navbar navbar-inverse navbar-static-top\" role=\"navigation\">\n".
+ "\n".
+ "    <div class=\"navbar-header\">\n".
+ "        <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">\n".
+ "        <span class=\"sr-only\">Toggle navigation</span>\n".
+ "        <span class=\"icon-bar\"></span>\n".
+ "        <span class=\"icon-bar\"></span>\n".
+ "        <span class=\"icon-bar\"></span>\n".
+ "        </button>\n".
+ "    </div>\n".
+ "\n".
+ "    <div class=\"collapse navbar-collapse navbar-ex1-collapse\">\n".
+ "\n".
+ "        <ul class=\"nav navbar-nav\">\n".
+ "            <li><a href=\"./HomeORDashboard\">Dashboard</a></li>\n".
+ "            <li><a href=\"./profile\">Profile</a></li>\n".
+ "        </ul>\n".
+ "\n".
+ "        <div class=\"col-sm-3 col-md-3 pull-right\">\n".
+ "        <form class=\"navbar-form\" role=\"search\">\n".
+ "        <div class=\"input-group\">\n".
+ "            <input type=\"text\" class=\"form-control\" placeholder=\"Search\" name=\"srch-term\" id=\"srch-term\">\n".
+ "            <div class=\"input-group-btn\">\n".
+ "                <button class=\"btn btn-default\" type=\"submit\"><i class=\"glyphicon glyphicon-search\"></i></button>\n".
+ "            </div>\n".
+ "        </div>\n".
+ "        </form>\n".
+ "        </div>\n".
+ "\n".
+ "    </div>\n".
+ "</div>\n".
+ "\n".
+ "<div id=\"mySidenav\" class=\"sidenav\">\n".
+ "  <a href=\"javascript:void(0)\" class=\"closebtn\" onclick=\"closeNav()\">☓</a>\n".
+ "  <!-- Query Notifications -->\n".
+ "</div>\n".
+ "\n".
+ "<div id=\"main\">\n".
+ "  <h4>Sidenav</h4>\n".
+ "  <span style=\"font-size:14px;cursor:pointer\" onclick=\"openNav()\">१ Notifications</span>\n".
+ "</div>\n".
+ "\n".
+ "<script>\n".
+ "function openNav() {\n".
+ "    document.getElementById(\"mySidenav\").style.width = \"250px\".\n".
+ "    document.getElementById(\"main\").style.marginLeft = \"250px\".\n".
+ "}\n".
+ "\n".
+ "function closeNav() {\n".
+ "    document.getElementById(\"mySidenav\").style.width = \"0\".\n".
+ "    document.getElementById(\"main\").style.marginLeft= \"0\".\n".
+ "}\n".
+ "</script>\n".
+ "\n".
+ ">>>>>>> master\n".
+ "\n"
+ "</body>\n";
+
+?>
