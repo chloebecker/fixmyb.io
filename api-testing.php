486ce0367c21378366d82d27717e1785b03b1df9
Chloe Becker <chloe.a.becker@gmail.com>
2016-10-29 23:24:31 -0500

    twitter api sucess - ready for integration

--- api-testing.php
+++ api-testing.php
@@ -5,15 +5,15 @@ require_once('TwitterAPIExchange.php');
  
 #Set access tokens here
 $settings = array(
-    'oauth_access_token' => "804870278-QYktWwvRoD7G4L8GrFHswGkQXOjc0qJezOBctrgH",
-    'oauth_access_token_secret' => "V3xiypK19mEhY8exrbm7XnpXtj7SndRegYJ3yRboQEFnf",
-    'consumer_key' => "	yNVkcJwn2yPL5veEsXxv2q1Or",
-    'consumer_secret' => "jQc2bwk8m20eWt5j47cXTxsm7Wj9AGWP2VgGoW7prwqmoCeUOO"
+    'oauth_access_token' => "792463225463853061-P5ZDTgnzgkroQtYu3QkPR5wn9GSmfLA",
+    'oauth_access_token_secret' => "HQVGmAORW0RTZFDdg0CSWuRShQ7THCJnsAh0mPnOKNR6O",
+    'consumer_key' => "o7QBNAyTr2odzOdGSIrBENTEY",
+    'consumer_secret' => "uqafomtqM8XZTxt8gC7Sr7lmZfJIHjqnTUvJp4yVv74f7PwfQw"
 );
 
 $url = "https://api.twitter.com/1.1/users/lookup.json";
 $requestMethod = "GET";
-$getfield = "?screen_name=cbecker46"
+$getfield = "?screen_name=fixmybio";
 $twitter = new TwitterAPIExchange($settings);
 #json_decode -> decodes JSON string into associative array
 $string = json_decode($twitter->setGetfield($getfield)
@@ -21,14 +21,16 @@ $string = json_decode($twitter->setGetfield($getfield)
              ->performRequest(),$assoc = TRUE);
 
 #if Twitter returns an error...
-if($string["errors"][0]["message"] != "") {echo "<h3>Houston, we have a problem...</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+if($string['errors'][0]['message'] != '') {echo "<h3>Houston, we have a problem...</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
 
-echo "<pre>";
-print_r($string);
-echo "</pre>";
+### to see what the API returns ###
+#echo "<pre>";
+#print_r($string);
+#echo "</pre>";
 
-echo "Screen Name: ". $string['screen_name']."<br>"
-echo "User Bio: ". $string['description']."<br>"
+$userInfo = $string[0];
+#echo "Screen Name: ". $userInfo['screen_name']."<br>";
+echo "User Bio: ". $userInfo['description']."<br>";
 
 
 ?>
